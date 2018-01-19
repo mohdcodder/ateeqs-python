b2c9bfde1a79270658d125b9765b172f13ad19a6
mohdcodder <35587714+mohdcodder@users.noreply.github.com>
2018-01-19 10:37:54 +0530

    Add files via upload

---
+++ printing name.py
@@ -0,0 +1,5 @@
+        #MY FIRST PROGRAM
+print("Mohammed Ateeq")
+print("python is totally different from C")
+print("but this is fun")
+print("200+300")
---
+++ simple maths with python.py
@@ -0,0 +1,12 @@
+
+a=int(input("enter a value"))
+
+b=int(input("enter a value"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addition",c)
+print("subtraction",d)
+print("multiplication",e)
+print("division",f)
